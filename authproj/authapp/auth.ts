6196ad0a93ba8a3da210de8e7cd7a94a0dc9ddbd
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2024-01-24 02:55:04 -0500

    Mail feature in progress

--- authproj/authapp/auth.ts
+++ authproj/authapp/auth.ts
@@ -11,12 +11,12 @@ export const {
   signOut,
 } = NextAuth({
   pages: {
-    // redirect when somethign goes wrong
+    // redirect when something goes wrong
     signIn: "/auth/login",
     error: "/auth/error",
   },
   events: {
-    linkAccount: async ({ user }) => {
+    async linkAccount({ user }) {
       await db.user.update({
         where: { id: user.id },
         // check when the email was verified
@@ -26,24 +26,39 @@ export const {
       });
     },
   },
-  // SESSION
-  async session({ token, session }) {
-    console.log("session", session);
-    if (token.role && session.user) {
-      session.user.role = token.role as "ADMIN" | "USER";
-    }
-    return session;
-  },
-  // JWT
-  async jwt({ token }) {
-    // logged out
-    if (!token.sub) return token;
+  callbacks: {
+    // SIGN IN
+    async signIn({ user, account }) {
+      // allow Oauth WITHOUT VERIFICATION
+      if (account?.provider !== "credentials") return true;
+      // existing user
+      const existingUser = await getUserById(user.id);
+      if (!existingUser?.emailVerified) return false;
 
-    const existingUser = await getUserById(token.sub);
-    if (!existingUser) return token;
+      // add 2fa check
 
-    token.role = existingUser.role;
-    return token;
+      return true;
+    },
+
+    // SESSION
+    async session({ token, session }) {
+      console.log("session", session);
+      if (token.role && session.user) {
+        session.user.role = token.role as "ADMIN" | "USER";
+      }
+      return session;
+    },
+    // JWT
+    async jwt({ token }) {
+      // logged out
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+      return token;
+    },
   },
   adapter: PrismaAdapter(db), // Move adapter property inside the NextAuth configuration object
   session: { strategy: "jwt" },
