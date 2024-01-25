caa83989a87871d1d82fb1e64b3502ec38227855
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2024-01-25 12:36:31 -0500

    verification in progress

---
+++ authproj/authapp/app/auth/newVerification/page.tsx
@@ -0,0 +1,11 @@
+import { VerifyForm } from "../../../components/auth/VerificationForm";
+
+const NewVerificationPage = () => {
+  return (
+    <div>
+      <VerifyForm />
+    </div>
+  );
+};
+
+export default NewVerificationPage;
